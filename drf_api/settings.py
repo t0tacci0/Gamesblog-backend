ebf0093b5957d45a8df50aa0eb362294bd7bf2de
Antonio Cesarino <ant.ces86@outlook.com>
2024-08-12 22:12:31 +0000

    Add profiles app

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -36,7 +36,7 @@ SECRET_KEY = 'django-insecure-ng#_vecnx1k@6-cdjng39tkjc2a%ep^_qy0w_0n%k8iy0p-3mi
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['8000-t0tacci0-pp5backend-hxwgs98usa0.ws.codeinstitute-ide.net']
 
 
 # Application definition
@@ -50,6 +50,8 @@ INSTALLED_APPS = [
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
+
+    'profiles',
 ]
 
 MIDDLEWARE = [
@@ -134,3 +136,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = ['https://8000-t0tacci0-pp5backend-hxwgs98usa0.ws.codeinstitute-ide.net/']
