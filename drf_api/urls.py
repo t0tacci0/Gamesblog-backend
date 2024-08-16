041e7d14b52a05e669fb85ef6c8d9d1ce8c7ec73
Antonio Cesarino <ant.ces86@outlook.com>
2024-08-16 22:57:14 +0000

    Add permissions logout/login

--- drf_api/urls.py
+++ drf_api/urls.py
@@ -21,5 +21,6 @@ from django.views.generic.base import RedirectView
 urlpatterns = [
     path('', RedirectView.as_view(url='/profiles/', permanent=False)),
     path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
     path('', include('profiles.urls')),
 ]
