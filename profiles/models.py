debccf58ceeac3da8953a52862bbe50e6f6e85aa
Antonio Cesarino <ant.ces86@outlook.com>
2024-09-21 23:11:16 +0000

    Change default image avatar in profile

--- profiles/models.py
+++ profiles/models.py
@@ -10,7 +10,7 @@ class Profile(models.Model):
     name = models.CharField(max_length=255, blank=True)
     content = models.TextField(blank=True)
     image = models.ImageField(
-        upload_to='images/', default='../default_profile_ts9vg0'
+        upload_to='images/', default='../user_avatar_ztatjv'
     )
 
     class Meta:
